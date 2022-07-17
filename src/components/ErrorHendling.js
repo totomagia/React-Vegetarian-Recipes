b02ee75e3938ee80f3572b3a9142a3e7250184dd
totomagia <81924899+totomagia@users.noreply.github.com>
2022-07-17 11:27:07 +0200

    Delete src/components directory

--- src/components/ErrorHendling.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
- const ErrorHendling = () => {
-    return <h1> Sorry, this is a Vegetarian Website. Choose another Recipe or Ingredient!</h1>
- }
-
-export default ErrorHendling
